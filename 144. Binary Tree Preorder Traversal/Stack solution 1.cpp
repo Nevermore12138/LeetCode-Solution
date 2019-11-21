dd8407371f192d5e7bbaf3409c8e6c121226b75a
Zhipeng Xu <34062960+Nevermore12138@users.noreply.github.com>
2019-11-21 21:12:15 +0800

    Create Stack solution 1.cpp

---
+++ 144. Binary Tree Preorder Traversal/Stack solution 1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        stack<TreeNode *> nodeStack;
+        vector<int> result;
+        if(!root)
+            return result;
+        nodeStack.push(root);
+        while(!nodeStack.empty())
+        {
+            TreeNode* tempNode = nodeStack.top();
+            nodeStack.pop();
+            result.push_back(tempNode->val);
+            if(tempNode->right)
+                nodeStack.push(tempNode->right);
+            if(tempNode->left)
+                nodeStack.push(tempNode->left);
+            
+        }
+        return result;
+    }
+
+};
